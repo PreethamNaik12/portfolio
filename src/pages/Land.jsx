426bdccc2cca24e62b8f91d4814450e0a27e5314
Preetham Naik <99126947+PreethamNaik12@users.noreply.github.com>
2024-05-25 19:22:57 +0000

    add: project and about me

--- src/pages/Land.jsx
+++ src/pages/Land.jsx
@@ -1,9 +1,12 @@
 import Typography from '@mui/material/Typography'
-import { Button } from '@mui/joy';
+import { Button, Card, CardContent, Container } from '@mui/joy';
 import { Box } from '@mui/material';
 import { useEffect, useState } from 'react';
 import EastIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import AutoAwesomeIcon from '@mui/icons-material/SettingsSuggest';
+import GitHubIcon from '@mui/icons-material/GitHub';
 // import React from 'react';
+
 const imageUrls = [
     'worker.png',
     'worker(1).png',
@@ -16,12 +19,55 @@ const imageUrls = [
 ];
 
 const AboutMe = () => (
-    <>
-        <Typography variant="h1" color="initial">About Me</Typography>
-        <Typography variant="p" color="initial">
-        Embark on a journey through my path as a university student at NITK Surathkal. A self-taught web developer with an insatiable curiosity for machine learning, I bring a fusion of academic rigor and creative innovation to every project I undertake. With a dedication to continuous learning and a drive to explore the frontiers of technology, I thrive on pushing boundaries and exceeding expectations. Whether it's crafting captivating designs, diving deep into data science, or architecting cutting-edge solutions, I'm committed to making a meaningful impact through my work.
-        </Typography>
-    </>
+    <Box sx={{display:'flex', my:2}}>
+        <Box sx={{mx:5 }}>
+            <Typography variant="h2" color="initial" sx={{fontWeight:'600',fontFamily:'Gabarito'}}>About Me</Typography>
+            <Typography variant="h6" color="initial" sx={{mr:5, mt:5, textAlign:'justify'}}>
+            Embark on a journey through my path as a university student at NITK Surathkal. A self-taught web developer with an insatiable curiosity for machine learning, I bring a fusion of academic rigor and creative innovation to every project I undertake. With a dedication to continuous learning and a drive to explore the frontiers of technology, I thrive on pushing boundaries and exceeding expectations. Whether it's crafting captivating designs, diving deep into data science, or architecting cutting-edge solutions, I'm committed to making a meaningful impact through my work.
+            </Typography>
+        </Box>
+        <img src="/jake-nackos-Ga7aBzN7qDw-unsplash.jpg" alt="" style={{width:'400px', borderRadius:'10px', mx:2}} />
+    </Box>
+)
+const MyWork = () => (
+    <Box sx={{backgroundColor:'#f0f0f0', minHeight:'50vh', maxWidth:'75%', borderRadius:'13px', mb:4}}>
+        <Button disabled variant="plain" size='lg' startDecorator={<AutoAwesomeIcon color="primary" sx={{fontSize:'50px'}} />} sx={{m:1}} />
+        <Card variant="outlined" sx={{mx:3, my:2}}>
+            <CardContent>
+                <Typography level="h5"  sx={{fontSize:'20px', fontWeight:600}}>Project 1</Typography>
+                <Typography>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis consequatur dignissimos, laboriosam alias, accusamus impedit, sint ut deleniti quis quae atque. Sequi, in explicabo. Cupiditate corporis ipsum iusto magnam accusamus.
+                </Typography>
+                <Box sx={{mt:1}}>
+                    <Button variant='soft' sx={{mr:1}}>Button</Button>
+                    <Button sx={{mr:1}}>Button</Button>
+                </Box>
+            </CardContent>
+        </Card>
+        <Card variant="outlined" sx={{mx:3, my:2}}>
+            <CardContent>
+                <Typography level="h5"  sx={{fontSize:'20px', fontWeight:600}}>Project 1</Typography>
+                <Typography>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis consequatur dignissimos, laboriosam alias, accusamus impedit, sint ut deleniti quis quae atque. Sequi, in explicabo. Cupiditate corporis ipsum iusto magnam accusamus.
+                </Typography>
+                <Box sx={{mt:1}}>
+                    <Button variant='soft' sx={{mr:1}}>Button</Button>
+                    <Button sx={{mr:1}}>Button</Button>
+                </Box>
+            </CardContent>
+        </Card>
+    </Box>
+)
+
+const Contact = () => (
+    <Box>
+        <Typography variant="h2" color="initial" sx={{fontWeight:'600',fontFamily:'Gabarito'}}>Get in Touch</Typography>
+        <Box sx={{display:'flex',flexDirection:'column' , justifyContent:'center', mt:2}}>
+            <Button variant='soft' startDecorator={<GitHubIcon/>} size='lg' sx={{m:2}}>GitHub</Button>
+            <Button variant='soft' startDecorator={<GitHubIcon/>} size='lg' sx={{m:2}}>LinkedIn</Button>
+            <Button variant='soft' startDecorator={<GitHubIcon/>} size='lg' sx={{m:2}}>Mail</Button>
+        </Box>
+    </Box>
 )
 
 const Land = () => {
@@ -44,7 +90,7 @@ const Land = () => {
     const [isHovering, setIsHovering] = useState(false);
 
     return (
-        <>
+        <Container maxWidth='xl'>
             <Box sx={{
                 height:'100vh',
                 display:'flex',
@@ -57,7 +103,7 @@ const Land = () => {
                 </Typography>
                 <Typography variant="h5" sx={{textAlign: 'center', fontFamily:'Gabarito', mt:2}}>
                     Hey there! I'm Preetham,<br/> a programming enthusiast weaving digital wonders.
-                </Typography>
+                </Typography>Typ
             
                 <Box sx={{display:'flex', mt:2}}>
                     {/* <Button variant="solid">Solid</Button> */}
@@ -65,7 +111,7 @@ const Land = () => {
                     <Button variant="plain" endDecorator={<EastIcon/>}>About Me</Button>
                     {/* <Button variant="plain">Plain</Button> */}
                 </Box>
-                <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, marginBottom:'3em' }}>
                     <img
                         src={isHovering ? 'work-from-home.png': imageUrls[currentImageIndex]}
                         alt={`Image ${currentImageIndex}`}
@@ -74,11 +120,22 @@ const Land = () => {
                         onMouseLeave={() => setIsHovering(false)}
                     />
                 </Box>
+                <img src='/scroll-bar.png' style={{width:'40px'}}/>
             </Box>
             <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
                 <AboutMe/>
             </Box>
-        </>
+
+            <Typography variant="h2" color="initial" sx={{fontWeight:'600',fontFamily:'Gabarito', textAlign:'left', ml:5}}>Projects</Typography>
+
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+                <MyWork/>
+            </Box>
+
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+                <Contact/>
+            </Box>
+        </Container>
     );
 };
 
